e1d268816897511cf041a53ee776ddc05513d007
welon0919 <welon0919@gmail.com>
2026-02-21 19:51:05 +0800

    Fixed issue with icon binary

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,15 @@
-use std::fs;
-
 use eframe::icon_data::from_png_bytes;
 use egui::{IconData, Style, Visuals};
 use whiteboard::WhiteboardApp;
-const ICON_PATH: &'static str = "assets/icon.png";
-fn load_icon(path: &str) -> Result<IconData, String> {
-    let png_bytes = fs::read(path).map_err(|err| err.to_string())?;
-    from_png_bytes(&png_bytes).map_err(|err| err.to_string())
+fn load_icon() -> Result<IconData, String> {
+    let png_bytes = include_bytes!("../assets/icon.png");
+    from_png_bytes(png_bytes).map_err(|err| err.to_string())
 }
 fn main() -> eframe::Result<()> {
-    let icon = load_icon(ICON_PATH).expect("Failed to load icon");
+    let icon = load_icon().expect("Failed to load icon");
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
+            .with_inner_size([1280.0, 800.0])
             .with_maximized(true)
             .with_icon(icon),
         ..Default::default()
